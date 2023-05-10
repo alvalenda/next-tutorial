98815c4fe909088f44d37eb434376ffb006a60b2
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-10 09:31:02 -0300

    feat(dashboard): test page component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface PageProps {}
+
+const Page: FC<PageProps> = () => {
+  return <div className="text-slate-200 text-7xl">Page</div>;
+};
+
+export default Page;
