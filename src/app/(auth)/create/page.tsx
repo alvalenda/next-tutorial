7c96f43a452f80ae4cbf2f4284ddea02a57f5cc3
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-12 18:35:46 -0300

    feat(create): add `page.tsx`

--- src/app/(auth)/create/page.tsx
+++ src/app/(auth)/create/page.tsx
@@ -3,7 +3,22 @@ import { FC } from "react";
 interface PageProps {}
 
 const page: FC<PageProps> = () => {
-  return <div className="text-7xl">create a new user</div>;
+  return (
+    <div className="text-xl w-1/4 h-1/2 mx-auto mt-60">
+      <form className="flex flex-col flex-wrap basis-1">
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      create a new user
+    </div>
+  );
 };
 
 export default page;
