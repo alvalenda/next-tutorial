1812cb25cb441917208d6f639ed0fcf2e8b6f5f4
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-13 12:40:36 -0300

    commented

---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+// import { ThemeProvider } from "next-themes";
+
+// interface ProvidersProps {
+//   children: React.ReactNode;
+// }
+
+// const Providers = ({ children }: ProvidersProps) => {
+//   return <ThemeProvider>{children}</ThemeProvider>;
+// };
+
+// export default Providers;
