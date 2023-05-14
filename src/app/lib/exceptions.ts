8f0f051b966df3d1cda83746e182c6a51270b89c
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-14 09:45:38 -0300

    chore(lib): deleting folder

--- src/app/lib/exceptions.ts
+++
@@ -1,6 +0,0 @@
-export class AuthRequiredException extends Error {
-  constructor(message = "Authentication is required to access this resource") {
-    super(message);
-    this.name = "AuthRequiredException";
-  }
-}
