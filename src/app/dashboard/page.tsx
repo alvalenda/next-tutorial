55e0369ff934882cf621bb3bfd8a5d4a7271aa51
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-14 09:04:24 -0300

    .

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,19 +2,17 @@ import axios from "axios";
 
 interface PageProps {}
 
-const Page = async () => {
+const Page = async ({}: PageProps) => {
   const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
   await wait(3000);
 
-  // throw new Error("Authorization failed");
-
   const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/todos/2"
+    "https://jsonplaceholder.typicode.com/todos/1"
   );
 
   return (
-    <div className="text-slate-200 text-5xl dark:text-red-500">
+    <div className="text-5xl dark:text-red-500">
       {JSON.stringify(data, null, 4)}
     </div>
   );
