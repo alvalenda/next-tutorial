2d992ee706ae840957e3a7dc0daf508d4816339e
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-23 15:10:58 -0300

    feat(api): add param to user get

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,16 @@
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  console.log("GET REQUEST");
+  const { searchParams } = new URL(req.url);
+  const mySearchParam = searchParams.get("search");
+
+  console.log(searchParams);
+  console.log("Search Param:", mySearchParam);
+
+  return new Response(JSON.stringify({ message: "Hello World" }), {
+    status: 200,
+    statusText: "OK",
+  });
 }
 
 export async function POST() {
