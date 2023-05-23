4cd26a686fa39047c7a47ff7a7c389aabe18c4e7
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-23 14:51:16 -0300

    feat(api): add log in get & post request

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,3 +1,9 @@
-export async function GET() {}
+import { NextRequest } from "next/server";
 
-export async function POST() {}
+export async function GET(req: NextRequest) {
+  console.log("GET REQUEST");
+}
+
+export async function POST() {
+  console.log("POST REQUEST");
+}
